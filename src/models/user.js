bd790c0c6ac21e60a94880b3a126692c165fefde
vinay-nb <vinaybhathg11@gmail.com>
2020-02-01 17:04:01 +0530

    auth token

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 //creating explicit schema
 const userSchema = new mongoose.Schema({
@@ -44,6 +45,12 @@ const userSchema = new mongoose.Schema({
         }
     }
 })
+
+userSchema.methods.generateAuthToken = async function () {
+    const user = this
+    const token = jwt.sign({_id:user._id.toString() }, 'thisismynewcourse')
+    return token
+}
 
 userSchema.statics.findByCredentials = async (email, password) => {
     const user = await User.findOne ({ email })
